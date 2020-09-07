84c5e22c0928a404f3c74bbc817f2be23b733de4
Hossein Safa <Hossein@Hosseins-MacBook-Pro.local>
2020-09-07 23:31:31 +0430

    from-Mac-Spyder

---
+++ commentClassifier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  7 23:01:30 2020
+
+@author: Hossein
+"""
+
+
+!git add "commentClassifier.py"
+!git commit -m "from-Mac-Spyder"
+!git push
