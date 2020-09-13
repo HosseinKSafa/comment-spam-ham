510722ad1362d29d8fcaa07b94b966b1e6904cdd
Hossein <hksafa@gmail.com>
2020-09-13 16:16:34 +0430

    from-Mac-Spyder

--- commentClassifier.py
+++ commentClassifier.py
@@ -17,24 +17,27 @@ import hazm as hm
 
 
 zoomitComments=pd.read_csv("C:/Users/h.safa/Downloads/Personal Files/DataScience/DataScience/Data Sets/commentTop500000.csv")
-zoomitComments.shape[0]
 zoomitComments.describe()
 zoomitComments.columns
 zoomitComments.head()
-
 zoomitComments=zoomitComments.drop(["ParentCommentid","UpdateDate2","CreateDate2","UpdatedByUserId","Name","Email"], axis=1)
 
+#def func1st(par):
+#    par.replace("<br />"," ")
+#    return len(par.split(" "))
 
-zoomitComments['Message'] = zoomitComments['Message'].agg(lambda x: x.replace("<br />"," "))
-zoomitComments['Message'] = zoomitComments['Message'].agg(lambda x: x.replace("<br>"," "))
+#zoomitComments['wordCount'] = zoomitComments['Message'].agg(func1st)
+#zoomitComments['Message'] = zoomitComments['Message'].agg(lambda x: x.replace("<br />"," "))
+#zoomitComments['Message'] = zoomitComments['Message'].agg(lambda x: x.replace("<br>"," "))
+#zoomitComments['wordCount'] = zoomitComments['Message'].agg(lambda x: len(x.rsplit(" ")))
 
 
-zoomitComments['Message']
+zoomitComments['Message'][499724].replace("<br />"," ")
 
 zoomitComments['word_count'] = zoomitComments['Message'].agg(lambda x: len(x.split(" ")))
 
 
-zoomitComments['Message']
+zoomitComments['Message'][2].replace("<br />"," ")
 
 #for i in range(0,zoomitComments.shape[0],1):
 #    zoomitComments['Message'][i]=zoomitComments['Message'][i].replace("<br />"," ")
