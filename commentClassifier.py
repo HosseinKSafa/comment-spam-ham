e925cfa5a7825f083ef9dc8bf7a3030eec6c5d5f
Hossein <hksafa@gmail.com>
2020-09-13 12:47:39 +0430

    from-Mac-Spyder

--- commentClassifier.py
+++ commentClassifier.py
@@ -17,13 +17,14 @@ import hazm as hm
 
 
 zoomitComments=pd.read_csv("C:/Users/h.safa/Downloads/Personal Files/DataScience/DataScience/Data Sets/commentTop500000.csv")
-zoomitComments.shape
+zoomitComments.shape[0]
 zoomitComments.describe()
 zoomitComments.columns
 zoomitComments.head()
 
 zoomitComments=zoomitComments.drop(["ParentCommentid","UpdateDate2","CreateDate2","UpdatedByUserId","Name","Email"], axis=1)
-zoomitComments['wordCount']=zoomitComments['Message'].agg(lambda x: len(x.split(" "))) 
 
-zoomitComments.columns
-zoomitComments.head()
+for i in range(0,zoomitComments.shape[0],1):
+    zoomitComments['Message'][i]=zoomitComments['Message'][i].replace("<br />"," ")
+
+
