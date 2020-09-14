6b5d0f7896bcfb05d8c389880151703a82facacf
Hossein <hksafa@gmail.com>
2020-09-14 17:52:16 +0430

    from-Mac-Spyder

--- commentClassifier.py
+++ commentClassifier.py
@@ -27,9 +27,17 @@ zoomitComments['Message']=zoomitComments['Message'].astype(str)
 zoomitComments['Message'] = zoomitComments['Message'].agg(lambda x: reg.sub('[<br />]',' ',x))
 zoomitComments['wordCount'] = zoomitComments["Message"].agg(lambda x: len(x.split(" ")))
 zoomitComments['charCount'] = zoomitComments["Message"].agg(lambda x: len(x))
+zoomitComments['Message'] = zoomitComments['Message'].agg(lambda x: reg.sub('\s+',' ',x))
 
 stopWords=hm.stopwords_list()
 zoomitComments['#_of_StopWords']=zoomitComments['Message'].agg(lambda x: len([w for w in x.split() if w in stopWords]))
 
 stemWords=hm.Stemmer()
-zoomitComments['Message']=zoomitComments['Message'].agg(lambda x: (" ")())
+zoomitComments['Message']=zoomitComments['Message'].agg(lambda x: (' ').join([stemWords.stem(w) for w in x.split()]))
+
+zoomitComments['Message'][2]
+
+
+ reg.findall(' ++','hossein   safa')
+
+
